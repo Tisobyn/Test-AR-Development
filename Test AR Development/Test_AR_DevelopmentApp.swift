bded7faf7ece2f22ee92e5e8e3d45a9b25de2e1e
Tiso <tisobyn@gmail.com>
2025-10-12 22:34:23 +0500

    Initial Commit

---
+++ Test AR Development/Test_AR_DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AR_DevelopmentApp.swift
+//  Test AR Development
+//
+//  Created by Yermek Sabyrzhan on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Test_AR_DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
